ee622276c5b52e001ec03747854c6ba5b0023df4
lepmin <143072063+lepmin@users.noreply.github.com>
2023-08-24 12:20:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)''')
+
+cursor.execute("INSERT INTO users (name) VALUES (?)", ('NAMETEST',))
+
+
+conn.commit()
+
+cursor.execute("SELECT name FROM users WHERE id = ?", (1,))
+result = cursor.fetchone()[0]
+
+cursor.close()
+conn.close()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +24,9 @@ def hello_world():
     return 'Hello, World! TESTTESTTEST'
 
 @app.route('/test')
-def hello_world():
+def test():
+    return 'WOW, TEST!'
+
+@app.route('/name')
+def get_name():
     return 'WOW, TEST!'
